38be4d834e813c469293da40a21165a88fc91581
G-Miralles <g.miralles@itecriocuarto.org.ar>
2020-11-10 18:58:14 -0300

    Interfaz creada

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DataFormComponent } from './components/data-form/data-form.component';
+import { DataComponent } from './components/data/data.component';
+import { DataListComponent } from './components/data-list/data-list.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DataFormComponent,
+    DataComponent,
+    DataListComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
